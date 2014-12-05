7bbe9583a1b0f897031c5f582deea3d1e0990df0
Adrien Thebo <git@somethingsinistral.net>
2014-12-05 13:50:17 -0800

    Initial commit

---
+++ lib/literate-markdown.rb
@@ -0,0 +1,68 @@
+require 'kramdown'
+
+module Literate
+  class Markdown
+    def initialize(path, opts = {:comment => '#', :keeptext => true})
+      @path = path
+      @comment = opts[:comment]
+      @keeptext = opts[:keeptext]
+    end
+
+    def code
+      doc.root.children.map do |section|
+        case section.type
+        when :header
+          header(section)
+        when :p
+          p(section)
+        when :blank
+          section.value if @keeptext
+        end
+      end.compact.join
+    end
+
+    def doc
+      Kramdown::Document.new(text)
+    end
+
+    def text
+      File.read(@path)
+    end
+
+    private
+
+    def header(element)
+      if @keeptext
+        "\n#{@comment} h#{element.options[:level]}. " + element.children.first.value + "\n"
+      end
+    end
+
+    def p(element)
+      istext = false
+      text = element.children.map do |c|
+        case c.type
+        when :text
+          istext = true
+          c.value
+        when :codespan
+          c.value
+        when :smart_quote
+          istext = true
+          "'"
+        else
+          c.value
+        end
+      end.join('')
+
+      if istext
+        if @keeptext
+          text.gsub(/^/, "#{@comment} ")
+        else
+          ''
+        end
+      else
+        text
+      end
+    end
+  end
+end
